bad58af1847de50e5a93c67440c28680bb932708
iamuhammadkhan <19494818+iamuhammadkhan@users.noreply.github.com>
2021-08-17 14:12:22 +0500

    Initial Commit

---
+++ HomeVPNUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HomeVPNUI
+//
+//  Created by Muhammad Khan on 8/17/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HomeVPNUI/HomeVPNUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeVPNUIApp.swift
+//  HomeVPNUI
+//
+//  Created by Muhammad Khan on 8/17/21.
+//
+
+import SwiftUI
+
+@main
+struct HomeVPNUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
